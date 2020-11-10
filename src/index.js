90aea490ff3af1f371d9ed9059486a44b84ba23c
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-11-10 18:21:15 -0300

    Setup Axios and organize folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import './views/style/index.css'
+import App from './views/components/App'
+import reportWebVitals from './utils/reportWebVitals'
 
 ReactDOM.render(
 	<React.StrictMode>
---
+++ src/views/components/App.js
@@ -0,0 +1,7 @@
+import '../style/App.css'
+
+function App() {
+	return <div>Hi</div>
+}
+
+export default App
