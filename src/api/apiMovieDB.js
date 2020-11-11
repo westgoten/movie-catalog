7767a60d6bf46e8552eb9293a0b39c34fc03504e
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-11-11 16:04:34 -0300

    Implement client-side API endpoints

--- src/api/apiMovieDB.js
+++ src/api/apiMovieDB.js
@@ -8,3 +8,25 @@ export const getMovies = (filter, page = 1) => {
 export const getMovieDetails = (id) => {
 	return axios.get(`/movie/${id}?api_key=${API_KEY}`)
 }
+
+export const getMovieCredits = (id) => {
+	return axios.get(`/movie/${id}/credits?api_key=${API_KEY}`)
+}
+
+export const getMovieImages = (id) => {
+	return axios.get(`/movie/${id}/images?api_key=${API_KEY}`)
+}
+
+export const getMovieRecommendations = (id, page = 1) => {
+	return axios.get(
+		`/movie/${id}/recommendations?api_key=${API_KEY}&page=${page}`
+	)
+}
+
+export const getMovieVideos = (id) => {
+	return axios.get(`/movie/${id}/videos?api_key=${API_KEY}`)
+}
+
+export const getConfiguration = () => {
+	return axios.get(`/configuration?api_key=${API_KEY}`)
+}
