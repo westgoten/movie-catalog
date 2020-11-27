a610ae44b58d5f2ea4af9cf4b00c584683e8faab
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-11-27 16:42:10 -0300

    Improve SideNavbar adding opacity layer to screen when it's open; Make possible to close SideNavbar when clicking on opacity layer or any of its links; Define componentAttributes constants to handle common cases of dynamic attribute insertion on components

---
+++ src/utils/consts/componentAttributes.js
@@ -0,0 +1,2 @@
+export const none = {}
+export const visible = { visible: '' }
--- src/views/components/SideNavbar.js
+++ src/views/components/SideNavbar.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { none, visible } from '../../utils/consts/componentAttributes'
 import '../style/css/SideNavbar.css'
 
 function SideNavbar() {
@@ -10,10 +11,7 @@ function SideNavbar() {
 			<i
 				class='fas fa-bars side-navbar-menu-icon'
 				onClick={openNavbar}></i>
-			<nav
-				className={`side-navbar ${
-					isNavbarOpen ? 'side-navbar-open' : ''
-				}`}>
+			<nav className='side-navbar' {...(isNavbarOpen ? visible : none)}>
 				<i
 					className='fas fa-times side-navbar-close-button'
 					onClick={closeNavbar}></i>
@@ -21,24 +19,31 @@ function SideNavbar() {
 					exact
 					to='/'
 					className='side-navbar-item'
-					activeClassName='side-navbar-item-selected'>
+					activeClassName='side-navbar-item-selected'
+					onClick={closeNavbar}>
 					MOVIES
 				</NavLink>
 				<NavLink
 					exact
 					to='/watchlist'
 					className='side-navbar-item'
-					activeClassName='side-navbar-item-selected'>
+					activeClassName='side-navbar-item-selected'
+					onClick={closeNavbar}>
 					MY WATCHLIST
 				</NavLink>
 				<NavLink
 					exact
 					to='/auth'
 					className='side-navbar-item'
-					activeClassName='side-navbar-item-selected'>
+					activeClassName='side-navbar-item-selected'
+					onClick={closeNavbar}>
 					SIGN IN
 				</NavLink>
 			</nav>
+			<div
+				className='side-navbar-opacity-layer'
+				onClick={closeNavbar}
+				{...(isNavbarOpen ? visible : none)}></div>
 		</>
 	)
 
