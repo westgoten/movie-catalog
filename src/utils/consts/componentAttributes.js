326960cbbe28f1117576f1387b49287eb17f30bb
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-12-16 23:14:26 -0300

    Toggle Header visibility when user is scrolling the page; Create Header component

--- src/utils/consts/componentAttributes.js
+++ src/utils/consts/componentAttributes.js
@@ -1,3 +1,4 @@
 export const NONE = Object.freeze({})
 export const VISIBLE = Object.freeze({ visible: '' })
+export const INVISIBLE = Object.freeze({ invisible: '' })
 export const OPENED = Object.freeze({ opened: '' })
--- src/views/components/Header.js
+++ src/views/components/Header.js
@@ -1,15 +1,38 @@
+import { useEffect, useState, useRef } from 'react'
 import useScreenSize from '../../utils/hooks/useScreenSize'
 import MobileSearchBar from './MobileSearchBar'
 import SearchBar from './SearchBar'
 import TopNavbar from './TopNavbar'
 import { SMALL_WIDTH } from '../../utils/consts/screenSizes'
+import { INVISIBLE, NONE } from '../../utils/consts/componentAttributes'
 import '../style/css/Header.css'
 
 function Header() {
+	const [scrollYOffset, setScrollYOffset] = useState(0)
+	const [isHeaderVisible, setHeaderVisible] = useState(true)
 	const isScreenSizeSmall = useScreenSize(SMALL_WIDTH)
+	const app = useRef(null)
+
+	useEffect(() => {
+		app.current = document.querySelector('.app')
+	}, [])
+
+	useEffect(() => {
+		const appNode = app.current
+		appNode.addEventListener('scroll', handleHeaderVisibility)
+
+		function handleHeaderVisibility() {
+			if (scrollYOffset > appNode.scrollTop) setHeaderVisible(true)
+			else if (scrollYOffset < appNode.scrollTop) setHeaderVisible(false)
+			setScrollYOffset(appNode.scrollTop)
+		}
+
+		return () =>
+			appNode.removeEventListener('scroll', handleHeaderVisibility)
+	})
 
 	return (
-		<header className='header'>
+		<header className='header' {...(isHeaderVisible ? NONE : INVISIBLE)}>
 			<div className='logo'>
 				<i className='fas fa-film logo-icon'></i>
 				<span className='logo-text'>Movie Catalog</span>
