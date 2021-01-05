7060104f97a583306f01767db7065a152569f109
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2021-01-04 22:27:38 -0300

    Add image and overview sections to MoviesCard component; Make changes to MoviesCard's style

--- src/views/components/MoviesCard.js
+++ src/views/components/MoviesCard.js
@@ -1,7 +1,18 @@
 import '../style/css/MoviesCard.css'
 
 function MoviesCard() {
-	return <div className='movies-card'>MoviesCard</div>
+	return (
+		<div className='movies-card'>
+			<img alt='Movie poster' className='movies-card-image' />
+			<div className='movies-card-overview'>
+				<span className='movies-card-name'>Jiu Jitsu</span>
+				<span className='movies-card-genres'>Action, Thriller</span>
+				<div>
+					<span></span>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default MoviesCard
