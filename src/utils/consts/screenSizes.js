3ea9db479ffb727aeae056036c1d26c5cc97b1b9
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2021-02-02 20:51:11 -0300

    Change SearchBar's placeholder text style; Change screenSize's small-width constant (css and js)

--- src/utils/consts/screenSizes.js
+++ src/utils/consts/screenSizes.js
@@ -1,4 +1,4 @@
 export const BIG_WIDTH = '1100px'
 export const MEDIUM_WIDTH = '970px'
-export const SMALL_WIDTH = '736px'
+export const SMALL_WIDTH = '780px'
 export const VERY_SMALL_WIDTH = '360px'
