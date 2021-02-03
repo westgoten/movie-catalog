6d7e8acb4618d7f216e17387d043ee9be57322d5
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2021-02-03 00:58:16 -0300

    Implement search on MobileSearchBar component

--- src/views/components/MobileSearchBar.js
+++ src/views/components/MobileSearchBar.js
@@ -1,9 +1,14 @@
 import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { SEARCH_PATH } from '../../utils/consts/routePaths'
 import { OPENED, NONE } from '../../utils/consts/componentAttributes'
 import OpacityLayer from './OpacityLayer'
 
 function MobileSearchBar() {
+	const history = useHistory()
+
 	const [isSearchOpen, setSearchOpen] = useState(false)
+	const [inputText, setInputText] = useState('')
 
 	return (
 		<>
@@ -20,9 +25,14 @@ function MobileSearchBar() {
 						<input
 							className='mobile-search-bar-input'
 							type='text'
+							onChange={handleInput}
+							value={inputText}
+							onKeyDown={handleConfirmation}
 							autoFocus
 						/>
-						<div className='mobile-search-bar-search'>
+						<div
+							className='mobile-search-bar-search'
+							onClick={search}>
 							<i className='fas fa-search mobile-search-bar-search-icon'></i>
 						</div>
 					</div>
@@ -36,12 +46,27 @@ function MobileSearchBar() {
 		</>
 	)
 
-	function openSearchBar() {
-		setSearchOpen(true)
-	}
-
 	function closeSearchBar() {
 		setSearchOpen(false)
+	}
+
+	function handleInput(event) {
+		setInputText(event.target.value)
+	}
+
+	function handleConfirmation(event) {
+		if (event.keyCode === 13) search()
+	}
+
+	function search() {
+		if (inputText.length > 0) {
+			closeSearchBar()
+			history.push(`${SEARCH_PATH}/${encodeURIComponent(inputText)}/1`)
+		}
+	}
+
+	function openSearchBar() {
+		setSearchOpen(true)
 	}
 }
 
